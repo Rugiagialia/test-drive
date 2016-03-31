42f52173bac736615a670ee8c301f125823687aa
Rugiagialia <rugiagialia@gmail.com>
2016-03-31 16:45:11 +0300

    Update box-demo.ps1

--- box-demo.ps1
+++ box-demo.ps1
@@ -13,6 +13,13 @@ Disable-UAC
 Update-ExecutionPolicy Unrestricted
 
 #region Windows Options
+Write-BoxstarterMessage "Setting Taskbar buttons to Never Combine..."
+$taskbarButtonsRegKey = 'hkcu:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+if ( ( ( Get-ItemProperty -path $taskbarButtonsRegKey ).TaskbarGlomLevel ) -Ne 2 )
+{
+    Set-ItemProperty -Path $taskbarButtonsRegKey -Name "TaskbarGlomLevel" -Value 00000002
+    Invoke-Reboot
+}
 Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen
 Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 Set-TaskbarOptions -Size Large -Lock -Dock Bottom
