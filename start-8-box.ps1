e77ce0f1f5d6a392fcef99cd37ff22e1158d6c8e
Rugiagialia <rugiagialia@gmail.com>
2016-04-01 23:34:17 +0300

    Update start-8-box.ps1

--- start-8-box.ps1
+++ start-8-box.ps1
@@ -31,8 +31,16 @@ REG LOAD HKU\DEFAULT C:\Users\Default\NTUSER.DAT
 Write-BoxstarterMessage "Making changes to Windows Explorer"
 REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v SeparateProcess /t REG_DWORD /d 1 /f
 REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSizeMove /t REG_DWORD /d 0 /f
+REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete" /v "Append Completion" /t REG_SZ /d YES /f
+REG ADD "HKU\DEFAULT\AppEvents\Schemes\Apps\Explorer\Navigating\.Current" /ve /t REG_EXPAND_SZ /d "" /f
+Write-BoxstarterMessage "Windows 8 navigation settings"
+REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v OpenAtLogon /t REG_DWORD /d 0 /f
+REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v DesktopFirst /t REG_DWORD /d 0 /f
+REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v MakeAllAppsDefault /t REG_DWORD /d 0 /f
+REG ADD "HKU\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v MonitorOverride /t REG_DWORD /d 0 /f
+Write-BoxstarterMessage "Unloading the default profile hive"
 REG UNLOAD HKU\DEFAULT
-Write-BoxstarterMessage "Default profile hive unloaded"
+
 #endregion
 
 # Disable annoying asking
